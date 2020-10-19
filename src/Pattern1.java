e47ff67c1d348cbae69d547fade50e7fe959d0b4
Raj <rajkamal11126@gmail.com>
2020-10-19 11:08:27 +0530

    added atleast one upper case

--- src/Pattern1.java
+++ src/Pattern1.java
@@ -6,9 +6,9 @@ public class Pattern1 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter password should be eight character");
+		System.out.println("Enter password atleast 1 upper case");
 		String password = scan.nextLine();
-		String regex = "^[a-zA-Z0-9]{8,}$";
+		String regex = "^(?=.*[A-Z]).{8,}$";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(password);
 		System.out.println(matcher.matches());
