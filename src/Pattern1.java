0beef2a11340be6c52abcdb4cda077dc880ce715
Raj <rajkamal11126@gmail.com>
2020-10-19 09:52:52 +0530

    added user email

--- src/Pattern1.java
+++ src/Pattern1.java
@@ -6,11 +6,11 @@ public class Pattern1 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter Last name");
-		String lname = scan.nextLine();
-		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		System.out.println("Enter email id");
+		String email = scan.nextLine();
+		String regex = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$";
 		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(lname);
+		Matcher matcher = pattern.matcher(email);
 		System.out.println(matcher.matches());
 	}
 
