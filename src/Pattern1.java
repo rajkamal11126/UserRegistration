8b484ba658b041e8a454126f4827d8e79b84c780
Raj <rajkamal11126@gmail.com>
2020-10-19 11:16:57 +0530

    added atleast one numeric case

--- src/Pattern1.java
+++ src/Pattern1.java
@@ -6,9 +6,9 @@ public class Pattern1 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter password atleast 1 upper case");
+		System.out.println("Enter password atleast 1 numeric case");
 		String password = scan.nextLine();
-		String regex = "^(?=.*[A-Z]).{8,}$";
+		String regex = "^(?=.*[0-9])(?=.*[A-Z]).{8,}$";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(password);
 		System.out.println(matcher.matches());
